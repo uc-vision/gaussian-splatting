0b34054ccae39e0d8ba95d887d0fd7f2b159f595
Oliver Batchelor <oliver.batchelor@canterbury.ac.nz>
2023-08-11 19:26:33 +1200

    Don't put stuff in output/

--- train.py
+++ train.py
@@ -150,12 +150,12 @@ def training(dataset, opt, pipe, testing_iterations, saving_iterations, checkpoi
                 torch.save((gaussians.capture(), iteration), scene.model_path + "/chkpnt" + str(iteration) + ".pth")
 
 def prepare_output_and_logger(args):    
-    if not args.model_path:
-        if os.getenv('OAR_JOB_ID'):
-            unique_str=os.getenv('OAR_JOB_ID')
-        else:
-            unique_str = str(uuid.uuid4())
-        args.model_path = os.path.join("./output/", unique_str[0:10])
+    # if not args.model_path:
+    #     if os.getenv('OAR_JOB_ID'):
+    #         unique_str=os.getenv('OAR_JOB_ID')
+    #     else:
+    #         unique_str = str(uuid.uuid4())
+    #     args.model_path = os.path.join("./output/", unique_str[0:10])
         
     # Set up output folder
     print("Output folder: {}".format(args.model_path))
