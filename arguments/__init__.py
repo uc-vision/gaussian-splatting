8ddc17784f7853f22cde76b674b08c98d4b128b8
Oliver Batchelor <saulzar@gmail.cm>
2023-08-12 22:32:57 +1200

    .

--- arguments/__init__.py
+++ arguments/__init__.py
@@ -70,11 +70,11 @@ class PipelineParams(ParamGroup):
 
 class OptimizationParams(ParamGroup):
     def __init__(self, parser):
-        self.iterations = 30_000
+        self.iterations = 60_000
         self.position_lr_init = 0.00016
         self.position_lr_final = 0.0000016
         self.position_lr_delay_mult = 0.01
-        self.position_lr_max_steps = 30_000
+        self.position_lr_max_steps = 60_000
         self.feature_lr = 0.0025
         self.opacity_lr = 0.05
         self.scaling_lr = 0.005
@@ -90,8 +90,8 @@ class OptimizationParams(ParamGroup):
 
         self.opacity_reset_interval = 2000
         self.densify_from_iter = 500
-        self.densify_until_iter = 25_000
-        self.densify_grad_threshold = 0.0001
+        self.densify_until_iter = 50_000
+        self.densify_grad_threshold = 0.0002
         super().__init__(parser, "Optimization Parameters")
 
 def get_combined_args(parser : ArgumentParser):
